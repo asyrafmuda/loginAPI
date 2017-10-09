c8acde298211de7de3aa27a0254d1f853971074e
Asyraf Muda <asyrafmuda10@gmail.com>
2017-10-09 21:37:54 +0800

    login API
    login API

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+//DB Credential
+
+$user = 'root';
+$pass = 'root';
+$db = 'myappv2';
+$host ='localhost';
+
+//connect to mysql
+
+$connection = mysqli_connect($host, $user, $pass, $db ) or die('Connection Error:' . mysqli_error($connection));
+
+//fetch data
+
+$sql = "SELECT * FROM user WHERE username='$username' and password='$password'";
+$result = mysqli_query($connection, $sql) or die('Error in selecting' . mysqli_error($connection));
+$rows = mysqli_num_rows($result);
+
+if ($rows == 1){
+	$response['success']= true;
+	$response['message'] = 'Login Successful';
+}else{
+	$response['success']= false;
+	$response['message']='login Failed';
+
+	}
+
+	echo json_encode($response);
+
+?>
